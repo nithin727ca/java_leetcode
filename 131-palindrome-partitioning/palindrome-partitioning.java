7924f70bb80a38756225c098cf84485ee2d32102
nithin727ca <nithincyril27@gmail.com>
2026-07-30 23:23:38 +0530

    Time: 8 ms (73.96%) | Memory: 65.2 MB (75.45%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<List<String>> result=new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        woodcutter(s,new ArrayList<>());
+        return result;
+    }
+    public void woodcutter(String s,List<String> subResult){
+        if(s.length()==0 || s==null){
+            result.add(new ArrayList<>(subResult));
+            return;
+        }
+        for(int i=1;i<=s.length();i++){
+            String cut=s.substring(0,i);
+            if(!isPalindrome(cut)){
+                continue;
+            }
+            subResult.add(cut);
+            woodcutter(s.substring(i,s.length()),subResult);
+            subResult.remove(subResult.size()-1);
+        }
+        return;
+    }
+    public boolean isPalindrome(String s){
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
